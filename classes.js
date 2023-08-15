0e821f67ece0547c7d4cffc0f2ffd8469dd4b3c0
Jeyhun Abilov <abilovv599@gmail.com>
2023-08-15 11:07:17 +0400

    refactored to function

--- classes.js
+++ classes.js
@@ -1,31 +1,69 @@
-class BankAccount {
-  constructor(customerName, customerPassword) {
-    let _customerName = customerName;
-    let _customerPassword = customerPassword;
-    let _balance = 0;
+const BankAccount = (customerName, customerPassword) => {
+  let _customerName = customerName;
+  let _customerPassword = customerPassword;
+  let _balance = 0;
 
-    this.authenticate = (inputPassword) => inputPassword === _customerPassword;
+  const authenticate = (inputPassword) => inputPassword === _customerPassword;
 
-    this.deposit = (amount) => {
-      if (amount > 0) {
-        _balance += amount;
-        return `Deposited $${amount}. New balance: $${_balance}`;
-      } else {
-        return "Invalid deposit amount.";
-      }
-    };
+  const deposit = (amount) => {
+    if (amount > 0) {
+      _balance += amount;
+      return `Deposited $${amount}. New balance: $${_balance}`;
+    } else {
+      return "Invalid deposit amount.";
+    }
+  };
 
-    this.withdraw = (amount) => {
-      if (amount > 0 && amount <= _balance) {
-        _balance -= amount;
-        return `Withdrawn $${amount}. New balance: $${_balance}`;
-      } else {
-        return "Insufficient funds or invalid withdrawal amount.";
-      }
-    };
+  const withdraw = (amount) => {
+    if (amount > 0 && amount <= _balance) {
+      _balance -= amount;
+      return `Withdrawn $${amount}. New balance: $${_balance}`;
+    } else {
+      return "Insufficient funds or invalid withdrawal amount.";
+    }
+  };
 
-    this.getBalance = () => `Current balance: $${_balance}`;
+  const getBalance = () => `Current balance: $${_balance}`;
 
-    this.getCustomerName = () => _customerName;
-  }
-}
+  const getCustomerName = () => _customerName;
+
+  return {
+    authenticate,
+    deposit,
+    withdraw,
+    getBalance,
+    getCustomerName,
+  };
+};
+
+// class BankAccount {
+//   constructor(customerName, customerPassword) {
+//     let _customerName = customerName;
+//     let _customerPassword = customerPassword;
+//     let _balance = 0;
+
+//     this.authenticate = (inputPassword) => inputPassword === _customerPassword;
+
+//     this.deposit = (amount) => {
+//       if (amount > 0) {
+//         _balance += amount;
+//         return `Deposited $${amount}. New balance: $${_balance}`;
+//       } else {
+//         return "Invalid deposit amount.";
+//       }
+//     };
+
+//     this.withdraw = (amount) => {
+//       if (amount > 0 && amount <= _balance) {
+//         _balance -= amount;
+//         return `Withdrawn $${amount}. New balance: $${_balance}`;
+//       } else {
+//         return "Insufficient funds or invalid withdrawal amount.";
+//       }
+//     };
+
+//     this.getBalance = () => `Current balance: $${_balance}`;
+
+//     this.getCustomerName = () => _customerName;
+//   }
+// }
